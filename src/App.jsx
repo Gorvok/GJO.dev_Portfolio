d3677bb7a1ff523810c909a260693d89a4ddb453
Gjovani Gorvokaj <ggorvokaj@gmail.com>
2024-04-28 14:37:57 -0400

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Index from "./pages/Index";
+import Index from "./pages/index.jsx";
 import HeroContact from "./components/HeroContact";
 import Navbar from "./components/Navbar.jsx";
 import Footer from "./components/Footer.jsx";
